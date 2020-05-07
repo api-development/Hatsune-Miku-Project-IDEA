83c4dfc430e4c207d6619c7fb84975910a955f63
Poom Gamer Ember <poomgamere@lightkrub.rf.gd>
2020-05-08 00:14:35 +0700

    Update Text_to_speech.py

--- Sound/Text_to_speech.py
+++ Sound/Text_to_speech.py
@@ -1,3 +1,4 @@
+# Import Module Library
 from gtts import gTTS
 from pydub import AudioSegment
 AudioSegment.converter = "C:\\ffmpeg\\bin\\ffmpeg.exe"
@@ -6,13 +7,13 @@ AudioSegment.ffprobe ="C:\\ffmpeg\\bin\\ffprobe.exe"
 from pydub.playback import play
 import playsound
 
+#Generate Sound File With Google Text To Speak
 tts = gTTS(text='Test Hatsune Miku', lang='en', slow=True)
 tts.save("input.mp3")
 
-
+# Customize Sound
 sound = AudioSegment.from_file('input.mp3', format="mp3")
 
-# Customize
 octaves = 0.25
 
 new_sample_rate = int(sound.frame_rate * (2.0 ** octaves))
@@ -23,6 +24,8 @@ hipitch_sound = hipitch_sound.set_frame_rate(44100)
 
 play(hipitch_sound)
 
+# Output Sound Customized
 hipitch_sound.export("output.mp3", format="mp3")
 
+# Auto Play Sound
 playsound.playsound("output.mp3", True)
