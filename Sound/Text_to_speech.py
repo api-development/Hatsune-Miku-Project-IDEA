e91b40545c4935d3c05d76b5a18a3aa7d4b62b4c
BlackBird <50193121+BlackBirdG@users.noreply.github.com>
2020-05-08 16:58:07 +0700

    BlackB
    Fix play time,and add os.remove to delete input file

--- Sound/Text_to_speech.py
+++ Sound/Text_to_speech.py
@@ -1,31 +1,25 @@
 # Import Module Library
 from gtts import gTTS
 from pydub import AudioSegment
-AudioSegment.converter = "C:\\ffmpeg\\bin\\ffmpeg.exe"
-AudioSegment.ffmpeg = "C:\\ffmpeg\\bin\\ffmpeg.exe"
-AudioSegment.ffprobe ="C:\\ffmpeg\\bin\\ffprobe.exe"
 from pydub.playback import play
-import playsound
+import os
 
 #Generate Sound File With Google Text To Speak
-tts = gTTS(text='Test Hatsune Miku', lang='en', slow=True)
+tts = gTTS(text='Miku, Miku, you can call me Miku', lang='en', slow=False)
 tts.save("input.mp3")
 
 # Customize Sound
 sound = AudioSegment.from_file('input.mp3', format="mp3")
 
+os.remove("input.mp3")  # removefile
+
 octaves = 0.25
 
-new_sample_rate = int(sound.frame_rate * (2.0 ** octaves))
+new_sample_rate = int(sound.frame_rate * (3.0 ** octaves))
 
 hipitch_sound = sound._spawn(sound.raw_data, overrides={'frame_rate': new_sample_rate})
 
 hipitch_sound = hipitch_sound.set_frame_rate(44100)
 
+#playsound
 play(hipitch_sound)
-
-# Output Sound Customized
-hipitch_sound.export("output.mp3", format="mp3")
-
-# Auto Play Sound
-playsound.playsound("output.mp3", True)
